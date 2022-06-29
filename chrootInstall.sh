8475e45ef68ed64110b3cbe3b56c5d04727ea47a
rwinkhart <cuan@mainrig.lan>
2022-06-29 02:58:39 -0400

    Added pacman hook to disable ModemManager DBUS service after upgrade

--- chrootInstall.sh
+++ chrootInstall.sh
@@ -73,6 +73,7 @@ chown "$username":"$users" /home/"$username"/.bashrc
 curl https://raw.githubusercontent.com/rwinkhart/artix-install-script/main/config-files/"$device"pacman.conf -o /etc/pacman.conf
 mkdir -p /etc/pacman.d/hooks
 curl https://raw.githubusercontent.com/rwinkhart/artix-install-script/main/config-files/paccache-clean-hook -o /etc/pacman.d/hooks/paccache-clean.hook
+curl https://raw.githubusercontent.com/rwinkhart/artix-install-script/main/config-files/modemmanager-hook -o /etc/pacman.d/hooks/modemmanager.hook
 if [ "$gpu" == 'NVIDIA' ]; then
     curl https://raw.githubusercontent.com/rwinkhart/artix-install-script/main/config-files/nvidia-hook -o /etc/pacman.d/hooks/nvidia.hook
 fi
