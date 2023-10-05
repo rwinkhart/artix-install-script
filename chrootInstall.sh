3e1d73059e3981d2ee8fa2b82903302a2ecb2ca2
Randall Winkhart <idgr@tutanota.com>
2023-10-04 20:27:53 -0400

    Use only the iGPU on first boot (g14)

--- chrootInstall.sh
+++ chrootInstall.sh
@@ -153,6 +153,7 @@ if [ "$formfactor" == 1 ]; then
     curl https://raw.githubusercontent.com/rwinkhart/artix-install-script/main/programs/NVIDIA-FCKR/NVIDIA-FCKR -o /usr/local/bin/NVIDIA-FCKR
     pacman -S mesa vulkan-icd-loader vulkan-radeon libva-mesa-driver libva-utils acpi_call --needed --noconfirm
     chmod 755 /etc/local.d/bashpower.start /usr/local/bin/NVIDIA-FCKR
+    NVIDIA-FCKR integrated
 fi
 
 # ssh configuration
