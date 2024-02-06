94f96b0b3cb9a084e297a04ed90b0bcb0252f1ba
Randall Winkhart <idgr@tutanota.com>
2024-02-06 00:44:02 -0500

    Install hunspell(-en_us) alongside Plasma

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 loadkeys us
 echo ----------------------------------------------------------------------------------------------
 echo rwinkhart\'s artix install script
-echo last updated february 06, 2024 \(rev. A\)
+echo last updated february 06, 2024 \(rev. B\)
 echo ----------------------------------------------------------------------------------------------
 echo You will be asked some questions before installation.
 echo -e "----------------------------------------------------------------------------------------------\n"
