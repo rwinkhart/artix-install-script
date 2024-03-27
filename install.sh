18362592fd8f311d9f5974815920e482b5198eea
Randall Winkhart <idgr@tutanota.com>
2024-03-27 02:31:03 -0400

    Re-enable custom panel and task tray configuration

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 loadkeys us
 echo ----------------------------------------------------------------------------------------------
 echo rwinkhart\'s Artix Install Script
-echo last updated March 26, 2024 \(rev. D\)
+echo last updated March 26, 2024 \(rev. E\)
 echo ----------------------------------------------------------------------------------------------
 echo You will be asked some questions before installation.
 echo -e "----------------------------------------------------------------------------------------------\n"
