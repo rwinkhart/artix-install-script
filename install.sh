31e204cff51528904d48633704734c215823e977
Randall Winkhart <idgr@tutanota.com>
2024-03-27 14:13:42 -0400

    Disable document history

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 loadkeys us
 echo ----------------------------------------------------------------------------------------------
 echo rwinkhart\'s Artix Install Script
-echo last updated March 27, 2024 \(rev. C\)
+echo last updated March 27, 2024 \(rev. D\)
 echo ----------------------------------------------------------------------------------------------
 echo You will be asked some questions before installation.
 echo -e "----------------------------------------------------------------------------------------------\n"
