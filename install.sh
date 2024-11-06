16d64c8452a0d503c1cc26430c38188066840313
Randall Winkhart <idgr@tutanota.com>
2024-11-05 19:42:31 -0500

    Switch "fastfetch" alias to emulate "neofetch" rather than "paleofetch"

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 loadkeys us
 echo ----------------------------------------------------------------------------------------------
 echo rwinkhart\'s Artix Install Script
-echo Last updated November 05, 2024 \(rev. A\)
+echo Last updated November 05, 2024 \(rev. B\)
 echo ----------------------------------------------------------------------------------------------
 echo You will be asked some questions before installation.
 echo -e "----------------------------------------------------------------------------------------------\n"
