7518c3cd023b6225baecb255b227435889677193
Randall Winkhart <idgr@tutanota.com>
2025-03-01 18:10:04 -0500

    Remove community repo from pacman.conf

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 loadkeys us
 echo ----------------------------------------------------------------------------------------------
 echo rwinkhart\'s Artix Install Script
-echo Last updated January 25, 2025 \(rev. A\)
+echo Last updated March 01, 2025 \(rev. A\)
 echo ----------------------------------------------------------------------------------------------
 echo You will be asked some questions before installation.
 echo -e "----------------------------------------------------------------------------------------------\n"
